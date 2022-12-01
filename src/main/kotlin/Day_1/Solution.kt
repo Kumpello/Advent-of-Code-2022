0d7b323c2f91226b0a08a06c50957a27de418070
Kumpel <bleidd04@gmail.com>
2022-12-01 09:26:09 +0100

    - adds day_1 first task solution

--- src/main/kotlin/Day_1/Solution.kt
+++
@@ -1,5 +0,0 @@
-package Day_1
-
-fun main(args: Array<String>) {
-
-}
---
+++ src/main/kotlin/day_1/Solution.kt
@@ -0,0 +1,22 @@
+package day_1
+
+import java.io.File
+
+fun main(args: Array<String>) {
+    System.out.println(getFoodList().max())
+}
+
+fun getFoodList(): IntArray {
+    val foodList: IntArray = IntArray(1000)
+    var currentElf = 0
+    val fileName = "src/main/resources/day_1/input"
+    File(fileName).forEachLine {line ->
+        if (line.isNotEmpty()) {
+            foodList[currentElf] = foodList[currentElf] + line.toInt()
+        } else {
+            currentElf++
+        }
+    }
+
+    return foodList
+}
